c95794bb22f0deb30c7187518c99776f638601fe
Meaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-09-28 13:30:21 -0400

    Update Main.java

--- CCC/y21/S2/Main.java
+++ CCC/y21/S2/Main.java
@@ -1,14 +1,26 @@
 package CCC.y21.S2;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in); // Scanner
-        boolean[][] grid = new boolean[in.nextInt()][in.nextInt()]; //Declare Array
-        for(int i=0;i<grid.length;i++){for(int j=0;j<grid[0].length;j++){grid[i][j]=false;}} // Initialize Array
-        int n=in.nextInt();while(n-->0){if('R'==in.next().charAt(0)){for(int j=0,row=in.nextInt()-1;j<grid[0].length;j++){grid[row][j]=!grid[row][j];}}else{for(int j=0,row=in.nextInt()-1;j<grid.length;j++){grid[j][row]=!grid[j][row];}}} // Do task
-        int total=0;for(int i=0;i<grid.length;i++){for(int j=0;j<grid[0].length;j++){if(grid[i][j])total++;}} // Check Total
-        System.out.println(total); //Output Result
+        Scanner in = new Scanner(System.in);
+        int[] x = new int[in.nextInt()];
+        int[] y = new int[in.nextInt()];
+        int n=in.nextInt();
+        while(n-->0){
+            if('R'==in.next().charAt(0)){
+                x[in.nextInt()-1]++;
+            } else {
+                y[in.nextInt()-1]++;
+            }
+        }
+        int total=0;
+        for(int i : x){
+            for(int j : y){
+                if((i+j)%2!=0) total++;
+            }
+        }
+        System.out.println(total);
     }
 }
+
